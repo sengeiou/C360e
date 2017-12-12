9cf0ee943838e8438570fa5f89fa70bbad12ddaf
Alex <610124860@qq.com>
2017-12-12 19:40:34 +0800

    before dataSync

--- alfredPosClient/src/main/java/com/alfredposclient/adapter/DevicesAdapter.java
+++ alfredPosClient/src/main/java/com/alfredposclient/adapter/DevicesAdapter.java
@@ -155,7 +155,7 @@ public class DevicesAdapter extends CustomAdapter {
 		
 		@Override
 		public void onClick(final View view) {
-			DialogFactory.commonTwoBtnDialog(App.getTopActivity(), con.getResources().getString(R.string.warning), "解除绑定之后将不能打印,是否解绑?", con.getResources().getString(R.string.cancel), con.getResources().getString(R.string.ok), null, new OnClickListener() {
+			DialogFactory.commonTwoBtnDialog(App.getTopActivity(), con.getResources().getString(R.string.warning), "Unassign will disconnect Printer!", con.getResources().getString(R.string.cancel), con.getResources().getString(R.string.ok), null, new OnClickListener() {
 				
 				@Override
 				public void onClick(View v) {
