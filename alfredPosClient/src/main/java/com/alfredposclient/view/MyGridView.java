14469044f7c98ef6fed94339c7d0c684aefae48a
Alex <610124860@qq.com>
2018-01-12 15:33:55 +0800

    new  gridview

---
+++ alfredPosClient/src/main/java/com/alfredposclient/view/MyGridView.java
@@ -0,0 +1,37 @@
+package com.alfredposclient.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.GridView;
+
+/**
+ * Created by Alex on 2018/1/11.
+ */
+
+public class MyGridView extends GridView {
+    public boolean isOnMeasure;
+    public MyGridView(Context context) {
+        super(context);
+    }
+
+    public MyGridView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+
+    public MyGridView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        isOnMeasure = true;
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        isOnMeasure = false;
+        super.onLayout(changed, l, t, r, b);
+    }
+}
