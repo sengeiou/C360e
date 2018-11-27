cb8b9054974479d09ac5ce53009e271a1d5045c6
Alex <610124860@qq.com>
2018-11-27 16:25:05 +0800

    update

--- alfredSelfHelp/src/main/java/com/alfredselfhelp/activity/MenuActivity.java
+++ alfredSelfHelp/src/main/java/com/alfredselfhelp/activity/MenuActivity.java
@@ -269,7 +269,7 @@ public class MenuActivity extends BaseActivity implements CheckListener {
                     ItemDetail itemDetail = CoreData.getInstance().getItemDetailById(orderDetail.getItemId());
                     RemainingStock remainingStock = RemainingStockSQL.getRemainingStockByitemId(itemDetail.getItemTemplateId());
                     if (remainingStock != null) {
-                        if(remainingStock.getQty() < orderDetail.getItemNum() + count){
+                        if(remainingStock.getQty() < count){
                             UIHelp.showSoShortToast(MenuActivity.this, "Out Of Stock");
                             cartAdater.notifyDataSetChanged();
                             return;
