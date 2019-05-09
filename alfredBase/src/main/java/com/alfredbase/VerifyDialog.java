6c5e653afc7d2531e1d346120decdda70260288b
chen'song <chens@moonearly.com>
2019-05-09 15:25:51 +0800

    update

--- alfredBase/src/main/java/com/alfredbase/VerifyDialog.java
+++ alfredBase/src/main/java/com/alfredbase/VerifyDialog.java
@@ -8,7 +8,6 @@ import android.widget.TextView;
 
 import com.alfredbase.global.CoreData;
 import com.alfredbase.javabean.User;
-import com.alfredbase.javabean.model.MainPosInfo;
 import com.alfredbase.utils.TextTypeFace;
 import com.alfredbase.utils.ToastUtils;
 import com.alfredbase.view.NumerickeyboardOne;
@@ -28,7 +27,7 @@ public class VerifyDialog extends Dialog implements KeyBoardClickListener {
 	 * 当前键盘输入对应的状态，0表示输入的员工ID，1表示输入的密码
 	 */
 	private int state = STATE_IN_ENTER_ID;
-	
+
 	private TextView tv_id_1;
 	private TextView tv_id_2;
 	private TextView tv_id_3;
@@ -43,17 +42,17 @@ public class VerifyDialog extends Dialog implements KeyBoardClickListener {
 	private String msgObject;
 	private Object obj;
 	private Context context;
-//	private String old_employee_ID;
+	//	private String old_employee_ID;
 //	private boolean hasLogined = false;
 	private TextTypeFace textTypeFace;
-	
+
 	public VerifyDialog(Context context,Handler handler) {
 		super(context, R.style.Dialog_verify);
 		this.handler = handler;
 		this.context = context;
 		init();
 	}
-	
+
 	private void init() {
 		View contentView = View.inflate(getContext(),
 				R.layout.dialog_input_manager_account, null);
@@ -96,7 +95,7 @@ public class VerifyDialog extends Dialog implements KeyBoardClickListener {
 			}
 		} else if (key.equals(context.getResources().getString(R.string.back))) {
 			if (state == STATE_IN_ENTER_PASSWORD) {
-				((TextView)(findViewById(R.id.tv_title))).setText(context.getResources().getString(R.string.manager_pwd));
+				((TextView)(findViewById(R.id.tv_title))).setText(context.getResources().getString(R.string.manager_id));
 				state = STATE_IN_ENTER_ID;
 				keyBuf.delete(0, keyBuf.length());
 				employee_ID = null;
@@ -121,13 +120,7 @@ public class VerifyDialog extends Dialog implements KeyBoardClickListener {
 				keyBuf.delete(0, key_len);
 				setPassword(keyBuf.length());
 				int id = Integer.parseInt(employee_ID);
-				User user=null;
-               if(msgObject.equals("DISCOUNT")||msgObject.equals("TRANSFERTABLE")||msgObject.equals("VOID")||msgObject.equals("EDIT_ITEM_ACTION")||msgObject.equals("BILL_ON_HOLD")||msgObject.equals("ENTERTAINMENT")){
-				   user = CoreData.getInstance().getUserByPassword(id);
-			   }else {
-				  user = CoreData.getInstance().getUserByEmpId(id);
-			   }
-
+				User user = CoreData.getInstance().getUserByEmpId(id);
 				Map<String, Object> resultObject = new HashMap<String, Object>();
 				resultObject.put("MsgObject", msgObject);
 				resultObject.put("Object", obj);
@@ -135,20 +128,20 @@ public class VerifyDialog extends Dialog implements KeyBoardClickListener {
 				if (user == null) {
 					ToastUtils.showToast((BaseActivity)context, context.getResources().getString(R.string.name_pwd_error));
 					((TextView) (findViewById(R.id.tv_title)))
-							.setText(context.getResources().getString(R.string.manager_pwd));
+							.setText(context.getResources().getString(R.string.manager_id));
 					state = STATE_IN_ENTER_ID;
 					employee_ID = null;
 					password = null;
 				}else {
 					if (user.getType() != ParamConst.USER_TYPE_MANAGER) {
 						ToastUtils.showToast((BaseActivity)context, context.getResources().getString(R.string.permission_insuff));
-						((TextView) (findViewById(R.id.tv_title))).setText(context.getResources().getString(R.string.manager_pwd));
+						((TextView) (findViewById(R.id.tv_title))).setText(context.getResources().getString(R.string.manager_id));
 						state = STATE_IN_ENTER_ID;
 						employee_ID = null;
 						password = null;
 					} else {
 						((TextView) (findViewById(R.id.tv_title)))
-								.setText(context.getResources().getString(R.string.manager_pwd));
+								.setText(context.getResources().getString(R.string.manager_id));
 						state = STATE_IN_ENTER_ID;
 						employee_ID = null;
 						password = null;
@@ -165,24 +158,24 @@ public class VerifyDialog extends Dialog implements KeyBoardClickListener {
 				Map<String, Object> resultObject = new HashMap<String, Object>();
 				resultObject.put("MsgObject", msgObject);
 				resultObject.put("Object", obj);
-				
+
 				if (user == null) {
 					ToastUtils.showToast((BaseActivity)context, context.getResources().getString(R.string.name_pwd_error));
 					((TextView) (findViewById(R.id.tv_title)))
-							.setText(context.getResources().getString(R.string.manager_pwd));
+							.setText(context.getResources().getString(R.string.manager_id));
 					state = STATE_IN_ENTER_ID;
 					employee_ID = null;
 					password = null;
 				}else {
 					if (user.getType() != ParamConst.USER_TYPE_MANAGER) {
 						ToastUtils.showToast((BaseActivity)context, context.getResources().getString(R.string.permission_insuff));
-						((TextView) (findViewById(R.id.tv_title))).setText(context.getResources().getString(R.string.manager_pwd));
+						((TextView) (findViewById(R.id.tv_title))).setText(context.getResources().getString(R.string.manager_id));
 						state = STATE_IN_ENTER_ID;
 						employee_ID = null;
 						password = null;
 					} else {
 						((TextView) (findViewById(R.id.tv_title)))
-						.setText(context.getResources().getString(R.string.manager_pwd));
+								.setText(context.getResources().getString(R.string.manager_id));
 						state = STATE_IN_ENTER_ID;
 						employee_ID = null;
 						password = null;
@@ -196,17 +189,17 @@ public class VerifyDialog extends Dialog implements KeyBoardClickListener {
 			}
 		}
 	}
-	
+
 	public void show(String msgObject,Object obj) {
 		this.msgObject = msgObject;
 		this.obj = obj;
 		super.show();
 	}
-	
+
 	@Override
 	public void dismiss() {
 		((TextView) (findViewById(R.id.tv_title)))
-		.setText(context.getResources().getString(R.string.manager_pwd));
+				.setText(context.getResources().getString(R.string.manager_id));
 		state = STATE_IN_ENTER_ID;
 		employee_ID = null;
 		password = null;
@@ -216,56 +209,56 @@ public class VerifyDialog extends Dialog implements KeyBoardClickListener {
 	}
 	private void setPassword(int key_len) {
 		switch (key_len) {
-		case 0: {
-			tv_id_1.setText("");
-			tv_id_2.setText("");
-			tv_id_3.setText("");
-			tv_id_4.setText("");
-			tv_id_5.setText("");
-			break;
-		}
-		case 1: {
-			tv_id_1.setText("*");
-			tv_id_2.setText("");
-			tv_id_3.setText("");
-			tv_id_4.setText("");
-			tv_id_5.setText("");
-			break;
-		}
-		case 2: {
-			tv_id_1.setText("*");
-			tv_id_2.setText("*");
-			tv_id_3.setText("");
-			tv_id_4.setText("");
-			tv_id_5.setText("");
-			break;
-		}
-		case 3: {
-			tv_id_1.setText("*");
-			tv_id_2.setText("*");
-			tv_id_3.setText("*");
-			tv_id_4.setText("");
-			tv_id_5.setText("");
-			break;
-		}
-		case 4: {
-			tv_id_1.setText("*");
-			tv_id_2.setText("*");
-			tv_id_3.setText("*");
-			tv_id_4.setText("*");
-			tv_id_5.setText("");
-			break;
-		}
-		case 5: {
-			tv_id_1.setText("*");
-			tv_id_2.setText("*");
-			tv_id_3.setText("*");
-			tv_id_4.setText("*");
-			tv_id_5.setText("*");
-			break;
-		}
-		default:
-			break;
+			case 0: {
+				tv_id_1.setText("");
+				tv_id_2.setText("");
+				tv_id_3.setText("");
+				tv_id_4.setText("");
+				tv_id_5.setText("");
+				break;
+			}
+			case 1: {
+				tv_id_1.setText("*");
+				tv_id_2.setText("");
+				tv_id_3.setText("");
+				tv_id_4.setText("");
+				tv_id_5.setText("");
+				break;
+			}
+			case 2: {
+				tv_id_1.setText("*");
+				tv_id_2.setText("*");
+				tv_id_3.setText("");
+				tv_id_4.setText("");
+				tv_id_5.setText("");
+				break;
+			}
+			case 3: {
+				tv_id_1.setText("*");
+				tv_id_2.setText("*");
+				tv_id_3.setText("*");
+				tv_id_4.setText("");
+				tv_id_5.setText("");
+				break;
+			}
+			case 4: {
+				tv_id_1.setText("*");
+				tv_id_2.setText("*");
+				tv_id_3.setText("*");
+				tv_id_4.setText("*");
+				tv_id_5.setText("");
+				break;
+			}
+			case 5: {
+				tv_id_1.setText("*");
+				tv_id_2.setText("*");
+				tv_id_3.setText("*");
+				tv_id_4.setText("*");
+				tv_id_5.setText("*");
+				break;
+			}
+			default:
+				break;
 		}
 	}
 
