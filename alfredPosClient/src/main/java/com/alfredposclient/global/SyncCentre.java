9a6811f8d3826b1f47043da8bef095ba82bf908f
Arif Sukmawan <arif.sukmawan@cartenz.co.id>
2019-07-15 08:04:32 +0700

    endpoint to staging

--- alfredPosClient/src/main/java/com/alfredposclient/global/SyncCentre.java
+++ alfredPosClient/src/main/java/com/alfredposclient/global/SyncCentre.java
@@ -424,7 +424,8 @@ public class SyncCentre {
         if (App.instance.isDebug) {
 //			return "http://172.16.0.190:8087/alfred-api/" + relativeUrl;
             //  return "http://192.168.104.10:8083/alfred-api/" + relativeUrl;
-            return "http://172.16.3.168:8083/alfred-api/" + relativeUrl;
+//            return "http://172.16.3.168:8083/alfred-api/" + relativeUrl;
+            return "http://18.139.110.250/alfred-api/" + relativeUrl;
         } else if (App.instance.isOpenLog) {
 
             return "http://139.224.17.126/alfred-api/" + relativeUrl;
