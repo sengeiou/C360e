e1364f059c3ee29a09730cdd55b999d4b6350dc0
Alex <610124860@qq.com>
2019-07-15 14:55:08 +0800

    v37v5

--- alfredBase/src/main/java/com/alfredbase/BaseApplication.java
+++ alfredBase/src/main/java/com/alfredbase/BaseApplication.java
@@ -52,7 +52,7 @@ public class BaseApplication extends Application {
      */
 
     public static boolean isDebug = false ;    //	Debug开关 release的时候设置为false
-    public static boolean isOpenLog = true;    //	release 时设置为false
+    public static boolean isOpenLog = false;    //	release 时设置为false
 
     protected String APPPATH = "sunmi";// sunmi or google or alibaba;
 
